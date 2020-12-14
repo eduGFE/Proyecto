0cee4ce02866230a24e0a3615743496e206de88f
Eduardo <edu21061991@gmail.com>
2020-12-14 23:19:07 +0100

    AñadidoComentario

--- Proyecto/src/modelo/productos/producto_consola.java
+++ Proyecto/src/modelo/productos/producto_consola.java
@@ -1,5 +1,5 @@
 package modelo.productos;
-
+//Parte eduardo
 public class producto_consola {
 //Clase encargada de mostrar los mensajes de productos por consola
 	public static void main(String[] args) {
